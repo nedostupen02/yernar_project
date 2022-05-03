f635e503ffa27fac8c3b9da9fd817ecf6550182e
Ubuntu <ubuntu@ip-172-31-63-141.ec2.internal>
2022-05-03 08:17:55 +0000

    code

--- base/models.py
+++ base/models.py
@@ -10,9 +10,23 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(to='Category', on_delete=models.CASCADE, related_name='tasks', null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
         order_with_respect_to = 'user'
+
+class Tag(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
